51ffe043101dc7e8958fa0a4f377fe7a45041444
Iván Lutin <ivanlutin@gmail.com>
2014-08-05 14:47:35 -0600

    adding icons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(init);
 
+function init(){
+	$("#acordeon h3").click(function(){
+		$("#acordeon ul ul").slideUp();
+		if(!$(this).next().is(":visible")){
+			$(this).next().slideDown();
+			}
+
+	});
+
+}
